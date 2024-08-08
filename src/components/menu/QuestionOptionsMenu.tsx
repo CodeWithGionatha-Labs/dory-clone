ef60802e23f519c1a9d53bee3a8212120bd40929
gionny96 <gionathas@gmail.com>
2024-08-08 10:06:06 +0200

    update question options menu

--- src/components/menu/QuestionOptionsMenu.tsx
+++ src/components/menu/QuestionOptionsMenu.tsx
@@ -115,7 +115,6 @@ export const QuestionOptionsMenu = ({
         open={openDeleteDialog}
         onOpenChange={setOpenDeleteDialog}
         questionId={questionId}
-        onSuccess={() => {}}
       />
     </>
   );
