47ea4480aea42706a2b84b861fd613399c731ede
gionny96 <gionathas@gmail.com>
2024-08-16 15:41:23 +0200

    add comment

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,6 +5,7 @@ import colors from "tailwindcss/colors";
 
 const prisma = new PrismaClient();
 
+// NOTE: IMPORTANT to replace with an user id registered in Kinde
 const myUserId = "kp_4fc8a0fbaaf04a0abbeac48eaa501595";
 
 const runSeed = async () => {
